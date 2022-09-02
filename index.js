260abf1035f4a6110e47dc745834565af790395c
Nick Laptop <45341924+nitnub@users.noreply.github.com>
2022-09-01 23:18:32 -0400

    add navbar

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ function Spa() {
       <NavBar />
       <UserContext.Provider value={{userName: 'Test User!'}}>
         <Route path="/" exact component={Home} />
-        <Route path="/createAccount/" exact component={CreateAccount} />
-        <Route path="/login/" exact component={Login} />
-        <Route path="/deposit/" exact component={Deposit} />
-        <Route path="/withdrwaw/" exact component={Withdraw} />
-        <Route path="/balance/" exact component={Balance} />
-        <Route path="/allData/" exact component={AllData} />
+        <Route path="/createAccount/" component={CreateAccount} />
+        <Route path="/login/" component={Login} />
+        <Route path="/deposit/" component={Deposit} />
+        <Route path="/withdraw/" component={Withdraw} />
+        <Route path="/balance/" component={Balance} />
+        <Route path="/allData/" component={AllData} />
       </UserContext.Provider>
     </HashRouter>
   );
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,15 +1,61 @@
 function NavBar() {
   return (
-    <>
-      <a href="#">BadBank</a>
-      <a href="#/CreateAccount/">Create Account</a>
-      <a href="#/Login/">Login</a>
-      <a href="#/Deposit/">Deposit</a>
-      <a href="#/Withdraw/">Withdraw</a>
-      <a href="#/Balance/">Balance</a>
-      <a href="#/AllData/">AllData</a>
-    </>
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Bad Bank
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/CreateAccount/">
+                Create Account
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/Login/">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/Deposit/">
+                Deposit
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/Withdraw/">
+                Withdraw
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/Balance/">
+                Balance
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/AllData/">
+                AllData
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
-
-
