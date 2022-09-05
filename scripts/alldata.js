f37ca7663e9cafba45c2c34acacdf74191c62488
Nick Laptop <45341924+nitnub@users.noreply.github.com>
2022-09-04 23:43:44 -0400

    add table template to allData

--- scripts/alldata.js
+++ scripts/alldata.js
@@ -1,8 +1,51 @@
 function AllData() {
   const ctx = React.useContext(UserContext);
+  console.log(ctx.users)
   return (
-    <div>
-      <h1>AllData</h1>
+
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+
+
+
+    
+  );
+}
+
+
+
+{/* <div>
+      <h1 className="section-header">All Data</h1>
       {ctx.users.map((user) => (
         <>
           <div className="card">
@@ -30,15 +73,13 @@ function AllData() {
                 </tbody>
               </table>
             </div>
-          </div>
+          </div> */}
 
           {/* <ul>
             <li key={user.id}>
               <div>{`ID: ${user.id} || User: ${user.name} || Email: ${user.email} || Password: ${user.password}`}</div>
             </li>
           </ul> */}
-        </>
-      ))}
-    </div>
-  );
-}
+        // </>
+      // ))}
+    {/* </div> */}
